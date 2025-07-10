557a75eeb0a9b2ccdf6fe3fc522e9bbba4962bd8
avinashgadi <avinashgadi25@gmail.com>
2025-07-10 16:06:55 +0530

    cors

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ dotenv.config()
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRoute.js";
+import cors from "cors"
+dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 const DB_USER=process.env.DB_USER;
 const PASSWORD=process.env.DB_PASSWORD;
@@ -12,4 +15,7 @@ mongoose.connect( `mongodb+srv://${DB_USER}:${PASSWORD}@cluster0.ea0md1f.mongodb
     console.log("Server started");
   });
 });
+// mongoose.connect('mongodb://localhost:27017/backend').then(()=>{
+
+// });
 app.use("/api/users", userRouter);
