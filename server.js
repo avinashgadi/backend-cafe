86dd134f637432b69453153e5135492df151db40
avinashgadi <avinashgadi25@gmail.com>
2025-07-17 14:45:41 +0530

    fixed aome errors in server and product user router

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-import dotenv from "dotenv";
-dotenv.config()
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
-import cors from "cors"
+import productRouter from "./routes/productRoute.js";
+import orderRouter from "./routes/orderRoute.js"
+import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const DB_USER=process.env.DB_USER;
-const PASSWORD=process.env.DB_PASSWORD;
-mongoose.connect( `mongodb+srv://${DB_USER}:${PASSWORD}@cluster0.ea0md1f.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
   app.listen(8080, () => {
     console.log("Server started");
   });
 });
-// mongoose.connect('mongodb://localhost:27017/backend').then(()=>{
-// app.listen(8080, () => {
-//     console.log("Server started");
-// });
-// });
+
+// mongoose
+//   .connect(
+//     `mongodb+srv://${dbuser}:${dbpass}@cluster0.qjxhv.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+//   )
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter)
+//hello World
